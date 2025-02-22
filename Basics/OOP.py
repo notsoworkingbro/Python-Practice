6d76cc0b5229dab0d6a971598cc7f84fd18f74fd
Notsoworkingbro <workingamap@gmail.com>
2025-02-22 19:28:42 +0800

    Initial

--- Basics/OOP.py
+++ Basics/OOP.py
@@ -3,10 +3,25 @@ class Parent():
     # Encapsulation
     def __init__(self):
         # __init__ >>> initial when program starts
-        self.name = "me"
+        self.name = 'me'
 
-    def printEverything(*args, **kwargs):
+    # Use for args and kwargs
+    def printEverything(self, *args, **kwargs):
+
         for (item, value) in kwargs.items():
             print(f"{item}: {value}")
         
         print(*args)
+
+class Child(Parent):
+
+    def hambog(self):
+        self.name = 'gay'
+        print(self.name)
+
+
+
+child = Child()
+child.hambog()
+# hi nak (args, non keyword arguments) --- shokoy = 'alessandro delumban', bading = 'alessandro', maniac = 'delumban' (keyword arguments)
+child.printEverything('Hi', 'nak', shokoy = 'alessandro delumban', bading = 'alessandro', maniac = 'delumban')
