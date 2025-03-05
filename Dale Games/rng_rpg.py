ef485c9c02bf11bf60347658c40bee0ec97ab8f1
Notsoworkingbro <workingamap@gmail.com>
2025-03-06 00:14:59 +0800

    rpg practice

--- Dale Games/rng_rpg.py
+++ Dale Games/rng_rpg.py
@@ -1,93 +1,85 @@
 import random
 
-# class Human():
+class Unit:
+    def __init__(self, name, health, armor, attack, movement_speed):
+        self.name = name
+        self.health = health
+        self.armor = armor
+        self.attack = attack
+        self.movement_speed = movement_speed
+
+    # Custom string representation output
+    def __str__(self):
+        return f"|Unit: {self.name} |\n|Health: {self.health}|\n|Armor: {self.armor}|\n|Attack: {self.attack}|\n|Movement Speed: {self.movement_speed}|"
+
+unit_lib = [
+    ("pawn",         *[10, 2, 3, 3]),
+    ("knight",         *[10, 2, 3, 3]),
+    ("paladin",         *[10, 2, 3, 3])
+]
+
+# class Attack:
 #     @staticmethod
-#     def pawn(health, armor, attack, movement_speed):
-#         health = health
-#         armor = armor
-#         attack = attack
-#         movement_speed = movement_speed
-
-#         print(health, armor, attack, movement_speed)
-
-#     @staticmethod
-#     def knight(health, armor, attack, movement_speed):
-#         health = health
-#         armor = armor
-#         attack = attack
-#         movement_speed = movement_speed
-
-#         print(health, armor, attack, movement_speed)
-
-# class Attack():
-#     @staticmethod
-#     def atk(health1, armor1, attack1, movement_speed1, health2, armor2, attack2, movement_speed2):
-#         if movement_speed1 > movement_speed2:
-#             health2 = health2 - attack1
+#     def atk(suppressed, aggressor):
+#         # If movement_speed1 > movement_speed2, pawn attacks knight
+#         if suppressed.movement_speed > aggressor.movement_speed:
+#             print('supressed flee')
 #         else:
-#             health1 = health1 - attack2
+#             # dyanamic code for ARMOR HP / ATTACK REDUCTION
+#             suppressed.health = suppressed.health - aggressor.attack + (suppressed.armor/10)
+#             print(suppressed.health)
 
-# pawn_base_Stat = [10, 2, 3, 3]
-# Knight_base_Stat = [50, 10, 15, 10]
+#         # Return the updated stats after the attack
+#         return suppressed, aggressor
 
-# human = Human()
-# human.pawn(*pawn_base_Stat)
+#     # mage stats coming soon
+#     @staticmethod
+#     def slow(pawn_stats, knight_stats):
+#         ...
 
-class Human():
-    @staticmethod
-    def pawn(health, armor, attack, movement_speed):
-        # Returns a dictionary for easier tracking of the stats
-        return {
-            'health': health,
-            'armor': armor,
-            'attack': attack,
-            'movement_speed': movement_speed
-        }
+# class Spawn:
+#     @staticmethod
+#     def spawn1():
+#         # Define the base stats and random unit selection
+#         unit_lib = [
+#             Unit("pawn",         *[10, 2, 3, 3]),
+#             Unit("knight",       *[50, 10, 15, 10]),
+#             Unit("paladdin",     *[50, 10, 15, 10])
+#         ]
+#         return unit_lib[random.randint(0, 2)]
 
-    @staticmethod
-    def knight(health, armor, attack, movement_speed):
-        return {
-            'health': health,
-            'armor': armor,
-            'attack': attack,
-            'movement_speed': movement_speed
-        }
+#     @staticmethod
+#     def spawn2():
+#         # Define the base stats and random unit selection
+#         unit_lib = [
+#             Unit("king",         *[50, 10, 15, 10]),
+#             Unit("archangel",    *[50, 10, 15, 10])
+#         ]
+#         return unit_lib[random.randint(0, 1)]
 
-class Attack():
-    @staticmethod
-    def atk(pawn_stats, knight_stats):
-        # If movement_speed1 > movement_speed2, pawn attacks knight
-        if pawn_stats['movement_speed'] > knight_stats['movement_speed']:
-            knight_stats['health'] -= pawn_stats['attack']
-        else:
-            pawn_stats['health'] -= knight_stats['attack']
+# spawned1 = Spawn.spawn1()
+# spawned2 = Spawn.spawn2()
 
-        # Return the updated stats after the attack
-        return pawn_stats, knight_stats
-
-
-
-# Define the base stats
-pawn_base_Stat = [10, 2, 3, 3]
-Knight_base_Stat = [50, 10, 15, 10]
+# Attack.atk(Spawn.spawn1(), Spawn.spawn2())
 
 # Initialize the Human class and get stats
+# print(pawn)
 
-pawn_stats = Human.pawn(*pawn_base_Stat)
-knight_stats = Human.knight(*Knight_base_Stat)
+# Unit: pawn
+# Health: 10
+# Armor: 2
+# Attack: 3
+# Movement Speed: 3
 
-# Print the initial stats
-print("Pawn Stats:", pawn_stats)
-print("Knight Stats:", knight_stats)
+# Attack.atk(pawn, knight)
+# Attack.atk(pawn, knight)
 
-# Perform an attack
-updated_pawn_stats, updated_knight_stats = Attack.atk(pawn_stats, knight_stats)
+# # Perform an attack
+# updated_pawn_stats, updated_knight_stats = Attack.atk(pawn_stats, knight_stats)
+# updated_pawn_stats, updated_knight_stats = Attack.atk(pawn_stats, knight_stats)
+# updated_pawn_stats, updated_knight_stats = Attack.atk(pawn_stats, knight_stats)
 
-updated_pawn_stats, updated_knight_stats = Attack.atk(pawn_stats, knight_stats)
-
-updated_pawn_stats, updated_knight_stats = Attack.atk(pawn_stats, knight_stats)
-
-print("After the attack:")
-print("Updated Pawn Stats:", updated_pawn_stats)
-print("Updated Knight Stats:", updated_knight_stats)
+# print("After the attack:")
+# print("Updated Pawn Stats:", updated_pawn_stats)
+# print("Updated Knight Stats:", updated_knight_stats)
 
